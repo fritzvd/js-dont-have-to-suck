4ec1b317b03d916c0ede341d14304f3e96580e3b
fritzvd <f.vandeventer@gmail.com>
2014-03-20 10:51:00 +0100

    added some reqs

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -3,14 +3,30 @@
 var app = angular.module('billing', ['restangular']);
 
 app.controller('Base', ["$scope", "Restangular", function ($scope, Restangular) {
-	Restangular.oneUrl('user/', 'http://localhost:3000/api/user').getList()
+	// Restangular.oneUrl('users', 'http://localhost:3000/api/user')
+	Restangular.allUrl('users', 'http://localhost:3000/api/user').getList()
 		.then(function(users) {
 			$scope.users = users;
-			$scope.users.route = 'user/' + users.id;
 		});
 
 	$scope.moreInfo = function (user) {
-		user.get();
-		console.info(user.bills);
+		user.oneUrl('users', 'http://localhost:3000/api/user/' + user.id).get()
+		.then(function(serverUser) {
+			$scope.users.filter(function (localUser){
+				if (serverUser.id == localUser.id) {
+					localUser = serverUser;
+					console.info($scope.users)
+					// $scope.users.push(serverUser)
+				}
+			})
+			// for (var i; i < $scope.users.length; i++) {
+			// 	console.info($scope.users, i);
+			// 	console.info($scope.users[i]);
+			// 	if ($scope.users[i].id == user.id) {
+			// 		$scope.users[i] = user;
+			// 		debugger
+			// 	}
+			// }
+		});
 	};
 }]);
